a2812ff4ce193f26308b7b13ab5bed31ddcd4878
Dtran54 <86464682+Dtran54@users.noreply.github.com>
2021-11-01 01:13:06 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ var a = 0;
 var s = 0;
 var w = 0;
 var r = 0;
+var c = 0;
+var d = 0;
 
 function preload() {
   soundFormats('mp3', 'wav');
@@ -10,24 +12,26 @@ function preload() {
   sound1 = loadSound('sounds/revolvercock.wav');
   sound2 = loadSound('sounds/shot.wav');
   sound3 = loadSound('sounds/whistle.wav');
+  sound4 = loadSound('sounds/horse.wav');
+  sound5 = loadSound('sounds/door.wav'); 
 }
 
 function setup() {
   createCanvas(400, 550);
-  a = color('#E91A00');
-  s = color('#E91A00');
+  a = color('#FFFFFF');
+  s = color('#FFFFFF');
   w = color('#FFFFFF');
-  r = color('#1600C9');
+  r = color('#FFFFFF');
+  c = color('#FFFFFF');
+  d = color('#FFFFFF');
 }
 
 
 function draw() {
   background('white');
-  let c = color('#E91A00');
   fill(c);
   noStroke();
   rect(20, 20, 360, 250);
-  let d = color('#1600C9');
   fill(d);
   noStroke();
   rect(20, 330, 360, 190);
@@ -55,19 +59,30 @@ function draw() {
 
 function mouseClicked() {
   counter += 1;
-  if (counter === 1) {
+  if (counter == 1) {
+    r = color('#1600C9');
+    d = color('#1600C9');
+    sound4.play();
+  }
+  if (counter === 2) {
+    c = color('#E91A00');
+    s = color('#E91A00');
+    a = color('#E91A00');
+    sound5.play();
+  }
+  if (counter === 3) {
     a = color('#000000');
     sound0.play();
   }
-  if (counter === 2) {
+  if (counter === 4) {
     s = color('#FFFFFF');
     sound1.play();
   }
-  if (counter === 3) {
+  if (counter === 5) {
     w = color('#000000');
     sound2.play();
   }
-  if (counter === 4) {
+  if (counter === 6) {
     r = color('#FFFFFF');
     sound3.play();
   } else {
